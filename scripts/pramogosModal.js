d00af83f5aac87cd682f3c0afcea7b7f94f5a421
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-06 10:25:22 +0200

    modal correction

--- scripts/pramogosModal.js
+++ scripts/pramogosModal.js
@@ -9,6 +9,7 @@ export default class PramogosModal {
     const modalBackground = document.createElement("div");
     modalBackground.setAttribute("class", "modal-background");
 
+
     const card = new PramoguKortele(this.cardData);
     card.classList.add("modal-content");
     card.classList.remove("hover-card");
@@ -17,12 +18,20 @@ export default class PramogosModal {
     modalBackground.append(card);
 
     const body = document.querySelector("body");
+    // body.classList.add("blocked-scrolling");
+
+    console.log(body.getBoundingClientRect().height);
+    console.log(window.scrollY);
+
+    // modalBackground.style.top = `${window.scrollY}`;
+
 
 
     // Modal closure event
 
     modalBackground.addEventListener("click", () => {
       modalBackground.remove();
+      // body.classList.remove("blocked-scrolling");
     });
 
 
